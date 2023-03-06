5e8977b37193f0f7f476d43695a3e22b27056e41
ruicore <hrui835@gmail.com>
2023-03-06 11:21:34 +0800

    beautiful code for organization

--- excelalchemy/types/value/organization.py
+++ excelalchemy/types/value/organization.py
@@ -13,9 +13,9 @@ class SingleOrganization(Radio):
 
     @classmethod
     def comment(cls, field_meta: FieldMetaInfo) -> str:
-        required = '必填' if field_meta.required else '非必填'
-        extra_hint = field_meta.hint or '需按照组织架构树填写组织完整路径，如“XX公司/一级部门/二级部门”.'
-        return f"""必填性：{required} \n提示：{extra_hint}"""
+        required_str = '必填' if field_meta.required else '非必填'
+        extra_hint = field_meta.hint or "需按照组织架构树填写组织完整路径，例如 'XX公司/一级部门/二级部门'."
+        return f"""必填性：{required_str}\n提示：{extra_hint}"""
 
     @classmethod
     def serialize(cls, value: Any, field_meta: FieldMetaInfo) -> Any:
@@ -47,27 +47,32 @@ class MultiOrganization(MultiCheckbox):
         return super().serialize(value, field_meta)
 
     @classmethod
-    def __validate__(cls, v: Any, field_meta: FieldMetaInfo) -> list[str]:
-        return super().__validate(v, field_meta)
-
-    @classmethod
     def deserialize(cls, value: str | list[str] | None | Any, field_meta: FieldMetaInfo) -> str | Any:
         if value is None or value == '':
             return ''
+
         if isinstance(value, str):
             return value
 
         if isinstance(value, list):
-            rst: list[str] = []
-            for id_ in value:
-                id_ = OptionId(id_)
-                try:
-                    rst.append(field_meta.options_id_map[id_].name)
-                except KeyError:
-                    logging.warning('无法找到组织 %s 的选项, 返回原值', id_)
-                    rst.append(id_)
-            # pyright: reportUnknownArgumentType=false
-            return f'{MULTI_CHECKBOX_SEPARATOR}'.join(rst)
+            option_names = cls.__option_ids_to_names__(value, field_meta)
+            return MULTI_CHECKBOX_SEPARATOR.join(map(str, option_names))
 
         logging.warning('%s 反序列化失败', cls.__name__)
         return value
+
+    @staticmethod
+    def __option_ids_to_names__(option_ids: list[str], field_meta: FieldMetaInfo) -> list[str]:
+        option_names: list[str] = []
+        for option_id in option_ids:
+            option_id = OptionId(option_id)
+            try:
+                option_names.append(field_meta.options_id_map[option_id].name)
+            except KeyError:
+                logging.warning('无法找到组织 %s 的选项, 返回原值', option_id)
+                option_names.append(option_id)
+        return option_names
+
+    @classmethod
+    def __validate__(cls, value: Any, field_meta: FieldMetaInfo) -> list[str]:
+        return super().__validate__(value, field_meta)

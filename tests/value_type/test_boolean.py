0503ca142047383cce6da4d14d2b5f37e3526697
何睿 <cyberserval@herui.local>
2023-04-25 15:49:33 +0800

    feat(date): impl test for date

--- tests/value_type/test_boolean.py
+++ tests/value_type/test_boolean.py
@@ -19,3 +19,17 @@ class TestBoolean(BaseTestCase):
             input_excel_name=FileRegistry.TEST_BOOLEAN_INPUT, output_excel_name='result.xlsx'
         )
         assert result.result == ValidateResult.SUCCESS, '导入失败'
+
+    async def test_boolean_deserialize(self):
+        class Importer(BaseModel):
+            is_active: Boolean = FieldMeta(label='是否启用', order=1)
+
+        alchemy = self.build_alchemy(Importer)
+        field = alchemy.ordered_field_meta[0]
+        assert field.value_type.deserialize(None, field) == '否'
+        assert field.value_type.deserialize(True, field) == '是'
+        assert field.value_type.deserialize(False, field) == '否'
+        assert field.value_type.deserialize('是', field) == '是'
+        assert field.value_type.deserialize('否', field) == '否'
+        assert field.value_type.deserialize('任何无法识别的值', field) == '任何无法识别的值'
+        assert field.value_type.deserialize('', field) == '否'
